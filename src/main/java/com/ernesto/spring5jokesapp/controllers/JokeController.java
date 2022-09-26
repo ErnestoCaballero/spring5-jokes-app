1077529dddc2e5fe1eabfc7c9d9764ba15adcaac
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-09-25 23:20:51 -0500

    delete unnecesary import from controller

--- src/main/java/com/ernesto/spring5jokesapp/controllers/JokeController.java
+++ src/main/java/com/ernesto/spring5jokesapp/controllers/JokeController.java
@@ -1,7 +1,6 @@
 package com.ernesto.spring5jokesapp.controllers;
 
 import com.ernesto.spring5jokesapp.services.JokeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +19,5 @@ public class JokeController {
         model.addAttribute("joke", jokeService.getJoke());
         return "index";
     }
-    
+
 }
